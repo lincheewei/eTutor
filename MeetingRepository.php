4d0f9c2d90720959091561161a38d8c68cd50034
khorjinho96 <khorjinho96@gmail.com>
2020-02-22 20:38:45 +0800

    update

--- MeetingRepository.php
+++ MeetingRepository.php
@@ -24,6 +24,30 @@
 
             return $meeting;
         }
+
+        public function getMeetingLast7($email) {
+            $result = array();
+            $today = new DateTime("now");
+            $today = $today->format("Y-m-d");
+            $lastDay = new DateTime("now");
+            $lastDay->sub(new DateInterval('P7D'));
+            $lastDay = $lastDay->format("Y-m-d");
+            $stmt = mysqli_stmt_init($this->databaseConnection);
+            if(mysqli_stmt_prepare($stmt, "SELECT COUNT(meeting_id) AS TotalMeeting FROM meeting WHERE (user_email = ? OR recipient_email = ?) AND DATE(meeting_date) <= ? AND DATE(meeting_date) > ?")){
+                mysqli_stmt_bind_param($stmt, "ssss", $value, $value, $today, $lastDay);
+                mysqli_stmt_bind_result($stmt, $totalMessage);
+                foreach($email as $value){
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_store_result($stmt);
+                    if(mysqli_stmt_num_rows($stmt) === 1){
+                        mysqli_stmt_fetch($stmt);
+                        $result[$value] = $totalMessage;
+                    }
+                }
+                mysqli_stmt_close($stmt);
+                return $result;
+            }
+        }
     }
 
 
